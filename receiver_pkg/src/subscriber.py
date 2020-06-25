222b0a59326d83b5352615c040830e296f3f26e5
Juan Carlos Garcia <memorexduo@gmail.com>
2020-06-25 23:36:57 +0000

    Initial Commit

---
+++ receiver_pkg/src/subscriber.py
@@ -0,0 +1,16 @@
+#l/usr/bin/env/python 
+
+import rospy
+from receiver_pkg.msg import Status
+
+def callback(data):
+    rospy.loginfo('El estado del Robot es {} - {}'.format(data.code, data.name))
+                  #'El estado del Robot es %d %s', (data.code, data.name)
+def listener():
+    rospy.init_node('my_listener', anonymous=True)
+    rospy.Subscriber('status_info', Status, callback)
+
+    rospy.spin()
+
+if __name__=='__main__':
+    listener()
